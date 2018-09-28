056d738a1134694f94e82baf9a7be49731a22d58
Matt Rutkowski <mrutkows@us.ibm.com>
2018-09-28 10:35:50 -0500

    Comment out Travis script contents as this was intended as an example only

--- travis/travis-test.sh
+++ travis/travis-test.sh
@@ -17,45 +17,45 @@
 set -e
 
 # OPEN_WHISK_KEY=From Travis env
-OPEN_WHISK_BIN=/home/ubuntu/bin
+OPEN_WHISK_KEY=/home/ubuntu/bin
 
-echo "Downloading wsk CLI...\n"
-WSK=https://openwhisk.ng.bluemix.net/cli/go/download/linux/amd64/wsk
-curl -O $WSK
-chmod u+x wsk
+# echo "Downloading wsk CLI...\n"
+# WSK=https://openwhisk.ng.bluemix.net/cli/go/download/linux/amd64/wsk
+# curl -O $WSK
+# chmod u+x wsk
 
-echo "Downloading wskdeploy CLI...\n"
-WSKDEPLOY=https://github.com/apache/incubator-openwhisk-wskdeploy/releases/download/latest/wskdeploy-latest-linux-amd64.tgz
-curl -OL $WSKDEPLOY
-tar xf wskdeploy-latest-linux-amd64.tgz
-chmod u+x wskdeploy
+# echo "Downloading wskdeploy CLI...\n"
+# WSKDEPLOY=https://github.com/apache/incubator-openwhisk-wskdeploy/releases/download/latest/wskdeploy-latest-linux-amd64.tgz
+# curl -OL $WSKDEPLOY
+# tar xf wskdeploy-latest-linux-amd64.tgz
+# chmod u+x wskdeploy
 
 
-export PATH=$PATH:`pwd`
+# export PATH=$PATH:`pwd`
 
-echo "Configuring CLI from apihost and API key"
-wsk property set --apihost openwhisk.ng.bluemix.net --auth $OPEN_WHISK_KEY > /dev/null 2>&1
+# echo "Configuring CLI from apihost and API key"
+# wsk property set --apihost openwhisk.ng.bluemix.net --auth $OPEN_WHISK_KEY > /dev/null 2>&1
 
-echo "Configure local.env..."
-touch local.env # Configurations defined in Travis, this is a no op
+# echo "Configure local.env..."
+# touch local.env # Configurations defined in Travis, this is a no op
 
-echo "Deploying application..."
-wskdeploy ../wskdeploy/manifest.yaml
+# echo "Deploying application..."
+# wskdeploy ../wskdeploy/manifest.yaml
 
-echo "Waiting for triggers/actions to finish installing (sleep 5)..."
-sleep 5
+# echo "Waiting for triggers/actions to finish installing (sleep 5)..."
+# sleep 5
 
-echo "Doing test..."
+# echo "Doing test..."
 
-if [[ true ]]
-then
-	echo "SUCCESS: Found the message we were expecting."
-    echo "Uninstalling..."
-    wskdeploy undeploy ../wskdeploy/manifest.yaml
-    exit 0
-else
-	echo "FAILURE: Something went wrong."
-	echo "Uninstalling..."
-	wskdeploy undeploy ../wskdeploy/manifest.yaml
-	exit -1
-fi
+# if [[ true ]]
+# then
+# 	echo "SUCCESS: Found the message we were expecting."
+#     echo "Uninstalling..."
+#     wskdeploy undeploy ../wskdeploy/manifest.yaml
+#     exit 0
+# else
+# 	echo "FAILURE: Something went wrong."
+# 	echo "Uninstalling..."
+# 	wskdeploy undeploy ../wskdeploy/manifest.yaml
+# 	exit -1
+# fi
